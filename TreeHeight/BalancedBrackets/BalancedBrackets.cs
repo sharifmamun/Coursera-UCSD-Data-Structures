8b2a5dd53d084a52ff8856909867273433b9af1f
sharif.mamun <sharif.mamun@gmail.com>
2017-04-21 15:19:55 -0700

    Week 1 - Check if the brackets are balanced or not using Stack

---
+++ TreeHeight/BalancedBrackets/BalancedBrackets.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedBrackets
+{
+
+    public class Bracket
+    {
+        public char Type;
+        public int Position;
+
+        public Bracket(char type, int position)
+        {
+            Type = type;
+            Position = position;
+        }
+
+        public bool Match(char c)
+        {
+            if (this.Type == '[' && c == ']')
+                return true;
+            if (this.Type == '{' && c == '}')
+                return true;
+            if (this.Type == '(' && c == ')')
+                return true;
+
+            return false;
+        }
+    }
+
+
+    class BalancedBrackets
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<Bracket> stack = new Stack<Bracket>();
+            bool isMatched = true;
+            int answer = 0;
+            int position;
+            for (position = 0; input != null && position < input.Length && isMatched; position++)
+            {
+                char c = input[position];
+
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    Bracket b = new Bracket(c, position+1);
+                    stack.Push(b);
+                }
+
+                if (c == ')' || c == '}' || c == ']')
+                {                    
+                    if (stack.Count > 0 && stack.Peek().Match(c))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        isMatched = false;
+                        answer = position + 1;
+                    }
+                }
+            }
+
+            if (stack.Count > 0 && isMatched)
+            {
+                isMatched = false;
+                if (stack.Count > 0)
+                {
+                    Bracket b = stack.Peek();
+                    answer = b.Position;
+                }                
+            }            
+
+            if (isMatched)
+                Console.WriteLine("Success");
+            else
+                Console.WriteLine(answer);
+
+        }
+    }
+}
